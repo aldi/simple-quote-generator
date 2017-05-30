946b59fa91183ecc0d1176c455726946e22345b7
aldiduzha <aldiduzha@users.noreply.github.com>
2017-05-30 14:46:06 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,23 @@
+﻿var url = "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
+var getQuote = function(data) {
+	$(".quote-text").text(data.quoteText);
+	var quot = 'https://twitter.com/intent/tweet?text=' + data.quoteText + ' Author: ' + data.quoteAuthor;
+	if (data.quoteAuthor === '') {
+		data.quoteAuthor = 'Unknown';
+	}
+	$(".quote-author").text('Author: ' + data.quoteAuthor);
+	$(".twitter-button").attr("href", quot);
+};
+
+//Initialize
+$(document).ready(function() {
+	$.getJSON(url, getQuote, 'jsonp');
+
+});
+
+//When clicking the new Quote button
+$("#quote").click(function() {
+	$(".quote-text").fadeOut(20);
+	$.getJSON(url, getQuote, 'jsonp');
+	$(".quote-text").fadeIn(1000);
+});
